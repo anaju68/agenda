9729c8b8c0956cd9cae14462068308386e6d5373
amandaanaju23 <125404603+amandaanaju23@users.noreply.github.com>
2023-04-06 16:56:08 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda</title>
+</head>
+<body>
+    <h1>Agenda do prof. Edney</h1>
+<?php
+include "conexao.php";
+
+
+
+$sql = "SELECT nome, sobrenome, telefone FROM agenda";
+$resultado = $conn->query($sql);
+
+if ($resultado->num_rows > 0) {
+    while ($linha = $resultado->fetch_assoc()) {
+        echo $linha["nome"] . " " . $linha["sobrenome"] . " - " . $linha["telefone"] . "<br>";
+    }
+} else {
+    echo "Nenhum registro encontrado.";
+}
+
+$conn->close();
+?>
+
+<a href="login.php">Inserir novo cadastro</a>
+
+    
+</body>
+</html>
+
